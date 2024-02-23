a40b9a6b7d71a23b92c297ae5ac99e004f128d54
haerimee <gofla728@gmail.com>
2024-02-23 13:22:27 +0900

    init project

---
+++ src/main/java/com/flab/readnshare/HelloController.java
@@ -0,0 +1,12 @@
+package com.flab.readnshare;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/")
+    public String hello(){
+        return "hello world";
+    }
+}
