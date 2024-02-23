d94a8a23f20677408b57a1424e968b7a202c4a62
haerimee <gofla728@gmail.com>
2024-02-23 13:48:50 +0900

    init project

---
+++ src/main/java/com/flab/readnshare/ReadnshareApplication.java
@@ -0,0 +1,13 @@
+package com.flab.readnshare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadnshareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadnshareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flab/readnshare/ReadnshareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flab.readnshare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadnshareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
