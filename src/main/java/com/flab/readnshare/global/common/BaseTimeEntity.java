f983649ac7383db93a9d07af7d451f0cc79fa977
haerimee <gofla728@gmail.com>
2024-03-06 18:15:15 +0900

    refactor: createdDate와 modifiedDate를 String에서 LocalDateTime으로 변경

--- src/main/java/com/flab/readnshare/global/common/BaseTimeEntity.java
+++ src/main/java/com/flab/readnshare/global/common/BaseTimeEntity.java
@@ -1,16 +1,12 @@
 package com.flab.readnshare.global.common;
 
-import jakarta.persistence.EntityListeners;
-import jakarta.persistence.MappedSuperclass;
-import jakarta.persistence.PrePersist;
-import jakarta.persistence.PreUpdate;
+import jakarta.persistence.*;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 
 @Getter
 @MappedSuperclass
@@ -18,19 +14,9 @@ import java.time.format.DateTimeFormatter;
 public abstract class BaseTimeEntity {
 
     @CreatedDate
-    private String createdDate;
+    @Column(updatable = false)
+    private LocalDateTime createdDate;
 
     @LastModifiedDate
-    private String modifiedDate;
-
-    @PrePersist // 엔티티 저장 전
-    public void onPrePersist(){
-        this.createdDate = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss"));
-        this.modifiedDate = this.createdDate;
-    }
-
-    @PreUpdate // 엔티티 수정 전
-    public void onPreUpdate(){
-        this.modifiedDate = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss"));
-    }
+    private LocalDateTime modifiedDate;
 }
