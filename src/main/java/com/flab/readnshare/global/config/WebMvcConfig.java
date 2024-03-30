972aea2f5bd621c38afd57a24f79323b4af92cd5
haerimee <gofla728@gmail.com>
2024-03-30 09:20:30 +0900

    feat: Argument Resolver 추가

--- src/main/java/com/flab/readnshare/global/config/WebMvcConfig.java
+++ src/main/java/com/flab/readnshare/global/config/WebMvcConfig.java
@@ -1,15 +1,23 @@
 package com.flab.readnshare.global.config;
 
+import com.flab.readnshare.domain.member.repository.MemberRepository;
 import com.flab.readnshare.global.common.auth.jwt.JwtAuthInterceptor;
+import com.flab.readnshare.global.common.auth.jwt.JwtUtil;
+import com.flab.readnshare.global.common.resolver.SignInMemberArgumentResolver;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @Configuration
 @RequiredArgsConstructor
 public class WebMvcConfig implements WebMvcConfigurer {
     private final JwtAuthInterceptor jwtAuthInterceptor;
+    private final JwtUtil jwtUtil;
+    private final MemberRepository memberRepository;
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
@@ -22,4 +30,8 @@ public class WebMvcConfig implements WebMvcConfigurer {
                 .excludePathPatterns("/api/member/signUp");
     }
 
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(new SignInMemberArgumentResolver(jwtUtil, memberRepository));
+    }
 }
