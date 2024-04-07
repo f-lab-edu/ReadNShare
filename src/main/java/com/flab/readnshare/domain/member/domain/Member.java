b3ce3d795c56d93b65e1d35a0474e9339eb16727
haerimee <gofla728@gmail.com>
2024-04-07 17:54:31 +0900

    feat: 팔로우 엔티티 작성(연관관계 설정)

--- src/main/java/com/flab/readnshare/domain/member/domain/Member.java
+++ src/main/java/com/flab/readnshare/domain/member/domain/Member.java
@@ -1,11 +1,14 @@
 package com.flab.readnshare.domain.member.domain;
 
+import com.flab.readnshare.domain.follow.domain.Follow;
 import com.flab.readnshare.global.common.BaseTimeEntity;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Entity
 @Getter
@@ -21,8 +24,14 @@ public class Member extends BaseTimeEntity {
     private String password;
     private String nickName;
 
+    @OneToMany(mappedBy = "fromMember")
+    private List<Follow> followings;
+
+    @OneToMany(mappedBy = "toMember")
+    private List<Follow> followers;
+
     @Builder
-    public Member(Long id, String email, String password, String nickName){
+    public Member(Long id, String email, String password, String nickName) {
         this.id = id;
         this.email = email;
         this.password = password;
