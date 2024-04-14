51403e55291cc3b15f71e2b0005b29752a8aa206
haerimee <gofla728@gmail.com>
2024-04-14 16:33:03 +0900

    refactor: Member에서 Follow 조회하지 않도록 삭제

--- src/main/java/com/flab/readnshare/domain/member/domain/Member.java
+++ src/main/java/com/flab/readnshare/domain/member/domain/Member.java
@@ -1,14 +1,11 @@
 package com.flab.readnshare.domain.member.domain;
 
-import com.flab.readnshare.domain.follow.domain.Follow;
 import com.flab.readnshare.global.common.BaseTimeEntity;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-import java.util.List;
 
 @Entity
 @Getter
@@ -23,12 +20,6 @@ public class Member extends BaseTimeEntity {
     private String email;
     private String password;
     private String nickName;
-
-    @OneToMany(mappedBy = "fromMember")
-    private List<Follow> followings;
-
-    @OneToMany(mappedBy = "toMember")
-    private List<Follow> followers;
 
     @Builder
     public Member(Long id, String email, String password, String nickName) {
