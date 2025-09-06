a682e4be835cc392eeb14c485f187921ce24b80c
haerimee <gofla728@gmail.com>
2025-09-06 15:28:06 +0900

    feat: 좋아요 수 엔티티 추가

---
+++ src/main/java/com/flab/readnshare/domain/like/domain/ReviewLikeCount.java
@@ -0,0 +1,46 @@
+package com.flab.readnshare.domain.like.domain;
+
+import com.flab.readnshare.domain.review.domain.Review;
+import com.flab.readnshare.global.common.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class ReviewLikeCount extends BaseTimeEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "review_like_count_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "review_id")
+    private Review review;
+
+    private Long count;
+
+    @Builder
+    public ReviewLikeCount(Review review, Long count) {
+        this.review = review;
+        this.count = count;
+    }
+
+    public static ReviewLikeCount create(Review review) {
+        return ReviewLikeCount.builder()
+                .review(review)
+                .count(0L)
+                .build();
+    }
+
+    public void increase() {
+        this.count++;
+    }
+
+    public void decrease() {
+        this.count--;
+    }
+}
